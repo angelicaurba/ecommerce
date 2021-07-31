bfd7440168a97b9ba3a46aff7b4a8fef45b2ad5c
AndreaBruno97 <andbruno97@gmail.com>
2021-07-31 16:15:23 +0200

    mail-service client (and its dependency in mail server)

--- mail-service/mail-server/build.gradle.kts
+++ mail-service/mail-server/build.gradle.kts
@@ -26,6 +26,10 @@ dependencies {
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-mail")
 
+    //adding internal modules dependency for dtos
+    implementation (project(":mail-service:mail-client"))
+    implementation(project(":common"))
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
 }
