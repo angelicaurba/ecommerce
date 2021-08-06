23b7ec2be9483357b2a5e2321a935dede2508816
Roberto Bressani <roberto.bressani97@gmail.com>
2021-08-06 17:08:50 +0200

    src: WalletService: improved entities

--- wallet-service/wallet-server/src/main/kotlin/it/polito/wa2/ecommerce/walletservice/domain/Wallet.kt
+++ wallet-service/wallet-server/src/main/kotlin/it/polito/wa2/ecommerce/walletservice/domain/Wallet.kt
@@ -1,22 +1,29 @@
 package it.polito.wa2.ecommerce.walletservice.domain
 
+import it.polito.wa2.ecommerce.common.EntityBase
+import it.polito.wa2.ecommerce.walletservice.client.CustomerWalletDTO
+import it.polito.wa2.ecommerce.walletservice.client.WalletDTO
+import it.polito.wa2.ecommerce.walletservice.client.WarehouseWalletDTO
 import java.math.BigDecimal
 import javax.persistence.*
 
 @Entity
-//TODO define entity base
 class Wallet(
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: Long? = null,
+    @Column(nullable=false)
     var owner:String,
+    @Column(nullable=false)
     var walletType: WalletType,
-    var amount: BigDecimal,
-    @OneToMany
-    var transactions:List<Transaction>
-
-){}
+    @Column(nullable=false)
+    //TODO study precision of bigdecimal
+    var amount: BigDecimal=BigDecimal("0.00")
+): EntityBase<Long>(){
+    fun toDTO(): WalletDTO {
+        return when(walletType){
+            WalletType.WAREHOUSE-> WarehouseWalletDTO(getId().toString(),owner, amount)
+            WalletType.CUSTOMER-> CustomerWalletDTO(getId().toString(), owner, amount)
+        }
+    }
+}
 
 enum class WalletType{
     CUSTOMER, WAREHOUSE
