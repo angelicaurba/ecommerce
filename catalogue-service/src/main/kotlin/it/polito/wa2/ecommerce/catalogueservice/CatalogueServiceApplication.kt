85d83803e377028871db2b597db7c75f3669ec50
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-08-16 12:16:30 +0200

    catalogue-service: add minimal gateway configuration

--- catalogue-service/src/main/kotlin/it/polito/wa2/ecommerce/catalogueservice/CatalogueServiceApplication.kt
+++ catalogue-service/src/main/kotlin/it/polito/wa2/ecommerce/catalogueservice/CatalogueServiceApplication.kt
@@ -2,9 +2,20 @@ package it.polito.wa2.ecommerce.catalogueservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class CatalogueServiceApplication
+class CatalogueServiceApplication{
+
+    @Bean
+    fun routes(builder: RouteLocatorBuilder): RouteLocator{
+        return builder
+            .routes()
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CatalogueServiceApplication>(*args)
