7e1b66ef20bf62d4f7d83ebf52b8478cd8bf94a5
Luca Giorgino <lucas.10m.lg@gmail.com>
2021-08-17 12:00:02 +0200

    added eureka server module

---
+++ eureka-server/src/test/kotlin/it/polito/wa2/ecommerce/eurekaserver/EurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.ecommerce.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
