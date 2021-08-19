95928ebb1ea468678f2b5f91cae972eac0ef245c
AndreaBruno97 <andbruno97@gmail.com>
2021-08-19 15:32:24 +0200

    use of common parseID() to convert userId from String to Long

--- user-service/user-server/src/main/kotlin/it/polito/wa2/ecommerce/userservice/controller/UserController.kt
+++ user-service/user-server/src/main/kotlin/it/polito/wa2/ecommerce/userservice/controller/UserController.kt
@@ -3,6 +3,7 @@ package it.polito.wa2.ecommerce.userservice.controller
 import it.polito.wa2.ecommerce.common.Rolename
 import it.polito.wa2.ecommerce.common.exceptions.BadRequestException
 import it.polito.wa2.ecommerce.common.exceptions.ForbiddenException
+import it.polito.wa2.ecommerce.common.parseID
 import it.polito.wa2.ecommerce.userservice.client.PasswordChangeRequest
 import it.polito.wa2.ecommerce.userservice.client.UserDetailsDTO
 import it.polito.wa2.ecommerce.userservice.service.impl.UserDetailsServiceImpl
@@ -26,19 +27,19 @@ class UserController {
     @GetMapping("/{userId}")
     @ResponseStatus(HttpStatus.OK)
     fun getUser(@PathVariable userId: String): UserDetailsDTO{
-        return userDetailsService.loadUserById(userId.toLong()).copy(password = null)
+        return userDetailsService.loadUserById(userId.parseID()).copy(password = null)
     }
 
     @GetMapping("/{userId}/email")
     @ResponseStatus(HttpStatus.OK)
     fun getEmail(@PathVariable userId: String): String{
-        return userDetailsService.loadUserEmailById(userId.toLong())
+        return userDetailsService.loadUserEmailById(userId.parseID())
     }
 
     @GetMapping("/{userId}/roles")
     @ResponseStatus(HttpStatus.OK)
     fun getRoles(@PathVariable userId: String): Set<Rolename>{
-        return userDetailsService.loadUserRolesById(userId.toLong())
+        return userDetailsService.loadUserRolesById(userId.parseID())
     }
 
     @PutMapping("/{userId}/password")
@@ -55,15 +56,13 @@ class UserController {
             throw BadRequestException("newPassword and confirmNewPassword should be equal")
         }
 
-        // TODO import extension function to parse userId to long with exception
-
         // TODO put in service
-        if(!userDetailsService.verifyPassword(userId.toLong(), request.oldPassword)){
+        if(!userDetailsService.verifyPassword(userId.parseID(), request.oldPassword)){
             throw ForbiddenException("User and password provided do not match")
         }
 
         // TODO check if userId can update (if it's the same as the JWT token user)
-        return userDetailsService.setPassword(userId.toLong(), request.newPassword)
+        return userDetailsService.setPassword(userId.parseID(), request.newPassword)
     }
 
     @PatchMapping("/{userId}/roles")
@@ -71,6 +70,6 @@ class UserController {
     fun changeRoles(@PathVariable userId: String){
 
         // TODO more general (give list of roles)
-        return userDetailsService.upgradeToAdmin(userId.toLong())
+        return userDetailsService.upgradeToAdmin(userId.parseID())
     }
 }
