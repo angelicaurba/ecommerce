eda80aeadc11f8d2195214df5a0496b544259450
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-08-23 17:30:09 +0200

    order-service: add generic type in OrderRequestDTO

--- order-service/order-client/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/client/OrderRequestDTO.kt
+++ order-service/order-client/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/client/OrderRequestDTO.kt
@@ -1,8 +1,8 @@
 package it.polito.wa2.ecommerce.orderservice.client
 
-data class OrderRequestDTO(
+data class OrderRequestDTO<T:ItemDTO>(
 //    val buyerId: Long, TODO lo mettiamo o lo leggiamo dal cookie? cookie
-//    val address: String, TODO lo mettiamo nel cookie o nell'ordine?
     val buyerWalletId: Long,
-    val deliveryItems: List<DeliveryItemDTO>
+    val address: String,
+    val deliveryItems: List<T>
 )
