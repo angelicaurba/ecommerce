c6a3f0e6daec5e2f4638fe0e9f5511965d9b2c8f
Roberto Bressani <roberto.bressani97@gmail.com>
2021-08-23 19:17:41 +0200

    src: added domain classes for saga pattern

---
+++ common/src/main/kotlin/it/polito/wa2/ecommerce/common/saga/domain/Emittable.kt
@@ -0,0 +1,5 @@
+package it.polito.wa2.ecommerce.common.saga.domain
+
+interface Emittable  {
+    fun getId(): String
+}
---
+++ common/src/main/kotlin/it/polito/wa2/ecommerce/common/saga/domain/OutboxEvent.kt
@@ -0,0 +1,29 @@
+package it.polito.wa2.ecommerce.common.saga.domain
+
+import it.polito.wa2.ecommerce.common.EntityBase
+import java.time.Instant
+import java.time.Instant.now
+import java.util.*
+import java.util.UUID.randomUUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "outbox", schema = "public")
+//@TypeDef(name = "jsonb", typeClass = JsonBinaryType::class)
+class OutboxEvent(
+    val timestamp: Instant = now(),
+
+    @Column(name = "aggregate_id", nullable = false)
+    val payloadId: String, //will contain the ID of the payload (emitted as key of the mesage)
+    @Column(name = "destination_topic", nullable = false)
+    val destinationTopic: String,
+    @Column(name = "payload", nullable = false, columnDefinition = "json")
+    val payload: String,
+    val type: String
+): EntityBase<Long>(){
+    @Column(name="event_id", nullable = false)
+    val eventId: String = randomUUID().toString()
+}
