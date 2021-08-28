a98202b46308d74f50b69dc3003b986157f0d0e0
Roberto Bressani <roberto.bressani97@gmail.com>
2021-08-28 20:40:49 +0200

    src: added repository for orderService

---
+++ order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/repository/OrderRepository.kt
@@ -0,0 +1,8 @@
+package it.polito.wa2.ecommerce.orderservice.it.polito.wa2.ecommerce.orderservice.repository
+
+import it.polito.wa2.ecommerce.orderservice.it.polito.wa2.ecommerce.orderservice.domain.Order
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface OrderRepository : PagingAndSortingRepository<Order, Long>
