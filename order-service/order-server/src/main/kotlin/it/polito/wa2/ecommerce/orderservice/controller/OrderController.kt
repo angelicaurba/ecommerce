c7e3f7632157007c6ff48679074464f13d05c55b
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-08-31 19:53:08 +0200

    order-server: implement controller

--- order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/controller/OrderController.kt
+++ order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/controller/OrderController.kt
@@ -1,12 +1,15 @@
 package it.polito.wa2.ecommerce.orderservice.controller
 
+import it.polito.wa2.ecommerce.common.exceptions.BadRequestException
+import it.polito.wa2.ecommerce.orderservice.client.UpdateOrderRequestDTO
+import it.polito.wa2.ecommerce.orderservice.client.item.PurchaseItemDTO
+import it.polito.wa2.ecommerce.orderservice.client.order.request.OrderRequestDTO
 import it.polito.wa2.ecommerce.orderservice.client.order.response.OrderDTO
 import it.polito.wa2.ecommerce.orderservice.service.OrderService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 import javax.validation.constraints.Min
 
 const val DEFAULT_PAGE_SIZE = "10"
@@ -15,9 +18,40 @@ const val DEFAULT_PAGE_SIZE = "10"
 @RequestMapping("/orders")
 class OrderController {
 
+    @Autowired lateinit var orderService: OrderService
+
     @GetMapping("/")
     fun getAllOrders(@RequestParam("pageIndex", defaultValue = "1") @Min(1) pageIdx: Int,
                      @RequestParam("pageSize", defaultValue = DEFAULT_PAGE_SIZE) @Min(1) pageSize: Int): List<OrderDTO>{
-        TODO("implement")
+        return orderService.getAllOrders(pageIdx, pageSize)
     }
+
+    @GetMapping("/{orderId}")
+    fun getOrderById(@PathVariable("orderId") orderId: String): OrderDTO{
+        return orderService.getOrderById(orderId)
+    }
+
+    @PostMapping("/")
+    fun addOrder(@RequestBody @Valid order: OrderRequestDTO<PurchaseItemDTO>,
+                 bindingResult: BindingResult
+    ): OrderDTO{
+        if (!bindingResult.hasErrors())
+            return orderService.addOrder(order)
+        throw BadRequestException("Bad order request")
+    }
+
+    @PatchMapping("/{orderId}")
+    fun updateOrder(@PathVariable("orderId") orderId: String,
+    @RequestBody @Valid updateOrderRequest: UpdateOrderRequestDTO,
+    bindingResult : BindingResult): OrderDTO{
+        if (!bindingResult.hasErrors())
+            return orderService.updateStatus(orderId, updateOrderRequest)
+        throw BadRequestException("Bad update request")
+    }
+
+    @DeleteMapping("/{orderId}")
+    fun cancelOrder(@PathVariable("orderId") orderId: String){
+        orderService.cancelOrder(orderId)
+    }
+
 }
