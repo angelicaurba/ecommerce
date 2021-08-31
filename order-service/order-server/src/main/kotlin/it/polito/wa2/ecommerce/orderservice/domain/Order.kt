e7279aa4a0ada9850d8517f1cb78be35e457672b
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-08-31 16:56:31 +0200

    order-server: add validation, implement toDTO and fix table name

--- order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/domain/Order.kt
+++ order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/domain/Order.kt
@@ -1,18 +1,51 @@
-package it.polito.wa2.ecommerce.orderservice.it.polito.wa2.ecommerce.orderservice.domain
+package it.polito.wa2.ecommerce.orderservice.domain
 
 import it.polito.wa2.ecommerce.common.EntityBase
+import it.polito.wa2.ecommerce.orderservice.client.item.PurchaseItemDTO
+import it.polito.wa2.ecommerce.orderservice.client.order.request.ItemsInWarehouseDTO
+import it.polito.wa2.ecommerce.orderservice.client.order.response.OrderDTO
 import it.polito.wa2.ecommerce.orderservice.client.order.response.Status
+
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.OneToMany
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
 
+@Table(name = "orders")
 @Entity
 class Order(
+    @Column @NotNull
     val buyerId: String,
+    @Column @NotNull
     val address: String,
+    @Column @NotNull
     val buyerWalletId: String,
-    @OneToMany() // TODO params?
-    val deliveryItems: List<PurchaseItem>,
+    @NotNull @OneToMany(mappedBy = "order")
+    val deliveryItems: Set<PurchaseItem>,
+    @Column @NotNull
     val status: Status
 ) : EntityBase<Long>() {
+    fun toDTO(): OrderDTO{
+        val itemsInWarehouseDTOList: List<ItemsInWarehouseDTO<PurchaseItemDTO>>
+
+        val warehouseItemsMap : Map<String, List<PurchaseItemDTO>> =
+        deliveryItems.groupBy ({it -> it.warehouseId!!}, {it -> PurchaseItemDTO(it.productId, it.amount, it.price)})
+
+        return OrderDTO(
+            buyerId,
+            address,
+            buyerWalletId,
+            warehouseItemsMap
+                .toList()
+                .map {
+                    ItemsInWarehouseDTO<PurchaseItemDTO>(
+                        it.first,
+                        it.second
+                    )
+                },
+            status
+        )
+    }
 }
 // TODO(add validation and columns)
