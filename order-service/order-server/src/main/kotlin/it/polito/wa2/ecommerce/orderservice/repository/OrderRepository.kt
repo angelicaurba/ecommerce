6cc8709636ddb4543abdc1a3d40ee655004ae940
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-08-31 19:51:26 +0200

    order-server: add method to repository

--- order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/repository/OrderRepository.kt
+++ order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/repository/OrderRepository.kt
@@ -1,8 +1,16 @@
 package it.polito.wa2.ecommerce.orderservice.repository
 
 import it.polito.wa2.ecommerce.orderservice.domain.Order
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
+import java.awt.print.Pageable
 
 @Repository
-interface OrderRepository : PagingAndSortingRepository<Order, Long>
+interface OrderRepository : PagingAndSortingRepository<Order, Long>{
+
+    @Transactional(readOnly = true)
+    fun findAllByBuyerId(buyerId : String, page: PageRequest): Page<Order>
+}
