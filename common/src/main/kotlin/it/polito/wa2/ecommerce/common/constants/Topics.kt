cf5d6fecc7054974ad2e71b85bec2f8dd39e3378
Roberto Bressani <roberto.bressani97@gmail.com>
2021-09-02 12:34:24 +0200

    src: added constants file

---
+++ common/src/main/kotlin/it/polito/wa2/ecommerce/common/constants/Topics.kt
@@ -0,0 +1,7 @@
+package it.polito.wa2.ecommerce.common.constants
+
+const val mailTopic = "mail"
+const val orderRequestTopic = "order-request"
+const val orderDetailsTopic = "order-details"
+const val orderStatusTopic = "order-status"
+const val paymentTopic = "payment-request"
