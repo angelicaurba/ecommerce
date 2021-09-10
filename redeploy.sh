6c12c03c4fa0a5ee988a44973462623b46d56a3b
AndreaBruno97 <andbruno97@gmail.com>
2021-09-10 20:07:12 +0200

    redeploy now correctly initializes mysql and debezium services

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 # if errors appear, run ecommerce->build setup->wrapper
 
 echo " "
@@ -53,20 +55,46 @@ echo " "
 echo 'starting docker-compose...'
 echo " "
 echo " "
+
 docker-compose build
 docker-compose up -d
 
+until docker exec -i mysql sh -c "exec mysql -uroot --password=admin " < ./databases/init.sql
+  do
+    sleep 5
+  done
 
-docker exec -i mysql sh -c "exec mysql -uroot --password=admin " < ./databases/init.sql
-
-# Configure debezium
-DEBEZIUM_CONFIG_FILE="./sample_debezium_config.json"
-DEBEZIUM_CONFIG="$(cat "$DEBEZIUM_CONFIG_FILE" | tr -d '\r' | tr -d '\n')"
-#echo "$DEBEZIUM_CONFIG" > TEST_FILE.txt
-
+# Debezium configuration
+DEBEZIUM_CONFIG_FILE='./sample_debezium_config.json'
+DEBEZIUM_CONFIG="$(cat "$DEBEZIUM_CONFIG_FILE" | tr -d '\r' | tr -d '\n' | tr -d ' ' )"
 
 # TODO update "database.include.list" and "table.include.list"
-echo " "
-docker exec kafka-connect curl -d "$DEBEZIUM_CONFIG" -X DELETE kafka-connect:8083/connectors/test-router
-echo " "
-docker exec kafka-connect curl -d "$DEBEZIUM_CONFIG" -H "Content-Type: application/json" -X POST kafka-connect:8083/connectors
+
+until output=$(docker exec kafka-connect curl -f kafka-connect:8083/connectors)
+  do
+    echo "Service is unavailable, sleeping..."
+    sleep 5
+  done
+
+#  example: $output =  ["test-router","test-router-2"]
+
+#  example: $output =  test-router,test-router-2
+output=$(echo $output | tr -d '[' | tr -d ']' | tr -d '"')
+
+# example: $output[0] = test-router | $output[1] = test-router-2
+IFS=',' read -a output <<< "$output"
+
+for connector in ${output[@]}
+do
+  until docker exec kafka-connect curl -i -f -X DELETE "kafka-connect:8083/connectors/""$connector"
+    do
+      echo "Route is unavailable, sleeping..."
+      sleep 5
+    done
+done
+
+until docker exec kafka-connect curl -i -f -d "$DEBEZIUM_CONFIG" -H "Content-Type:application/json" -X POST kafka-connect:8083/connectors
+    do
+      echo "Route is unavailable, sleeping..."
+      sleep 5
+    done
