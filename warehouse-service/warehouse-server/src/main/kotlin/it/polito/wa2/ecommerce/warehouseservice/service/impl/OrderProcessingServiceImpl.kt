88e7aae1858949b52fd49e8f005030ffa4b07d6b
Luca Giorgino <lucas.10m.lg@gmail.com>
2021-09-11 09:55:59 +0200

    commit to merge

--- warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/service/impl/OrderProcessingServiceImpl.kt
+++ warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/service/impl/OrderProcessingServiceImpl.kt
@@ -2,13 +2,10 @@ package it.polito.wa2.ecommerce.warehouseservice.service.impl
 
 import it.polito.wa2.ecommerce.common.saga.service.MessageService
 import it.polito.wa2.ecommerce.common.saga.service.ProcessingLogService
-import it.polito.wa2.ecommerce.orderservice.client.order.messages.OrderStatus
 import it.polito.wa2.ecommerce.warehouseservice.client.order.request.WarehouseOrderRequestDTO
 import it.polito.wa2.ecommerce.orderservice.client.order.messages.EventTypeOrderStatus
-import it.polito.wa2.ecommerce.warehouseservice.client.order.request.WarehouseOrderRequestDTO
-import it.polito.wa2.ecommerce.orderservice.client.order.messages.OrderStatus
 import it.polito.wa2.ecommerce.orderservice.client.order.messages.ResponseStatus
-import it.polito.wa2.ecommerce.orderservice.client.order.messages.Status
+import it.polito.wa2.ecommerce.orderservice.client.order.messages.OrderStatus
 import it.polito.wa2.ecommerce.warehouseservice.client.order.request.WarehouseOrderRequestCancelDTO
 import it.polito.wa2.ecommerce.warehouseservice.client.order.request.WarehouseOrderRequestNewDTO
 import it.polito.wa2.ecommerce.warehouseservice.service.OrderProcessingService
@@ -21,7 +18,7 @@ import java.util.*
 @Service
 @Transactional(propagation = Propagation.NESTED)
 class OrderProcessingServiceImpl: OrderProcessingService {
-    override fun process(orderRequestDTO: Any, id: String) {
+
 
 
     @Autowired
@@ -32,19 +29,8 @@ class OrderProcessingServiceImpl: OrderProcessingService {
 
     @Autowired
     lateinit var self: OrderProcessingService
-    // TODO commented function in order to make the project compile
-    // The original "process" override is commented,
-    // and empty override for all the functions are added
-
-    //    override fun process(orderRequestDTO: Any, id: String) {
-//        TODO("Not yet implemented")
-//    }
-    override fun process(orderRequestDTO: WarehouseOrderRequestDTO, id: String) {
-        TODO("Not yet implemented")
-    }
 
     override fun process(orderRequestDTO: WarehouseOrderRequestDTO, id: String){
-    override fun processOrderRequest(orderRequestDTO: WarehouseOrderRequestDTO): OrderStatus {
         val uuid = UUID.fromString(id)
         if(processingLogService.isProcessed(uuid))
             return
@@ -81,8 +67,8 @@ class OrderProcessingServiceImpl: OrderProcessingService {
 
         return OrderStatus(
             orderId, when (orderRequestDTO.requestType) {
-                OrderPaymentType.PAY -> Status.COMPLETED
-                OrderPaymentType.REFUND -> Status.REFUNDED
+                OrderPaymentType.PAY -> Req.COMPLETED
+                OrderPaymentType.REFUND -> OrderStatus.REFUNDED
             }, null
         )
     }
