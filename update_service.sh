a3bd97f5a94ac508e0273faf014745983b4aa0d2
AndreaBruno97 <andbruno97@gmail.com>
2021-09-12 16:05:28 +0200

    script that re-compiles and re-runs a single service (catalogue, mail, order, user, wallet, warehouse)

---
+++ update_service.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+parameters_num=1
+allowed_parameters=("catalogue" "mail" "order" "user" "wallet" "warehouse")
+
+if [ $# != $parameters_num ]; then
+  echo "You gave $# parameters instead of $parameters_num"
+  exit 1
+fi
+
+flag=false
+for allowed_elem in ${allowed_parameters[@]}; do
+  if [ $allowed_elem == $1 ]; then
+    flag=true
+    break
+  fi
+done
+
+if [ $flag == false ]; then
+  echo "Allowed parameters: ${allowed_parameters[@]}"
+  exit 2
+fi
+
+service=$1
+
+./gradlew :$service-service:$service-server:bootJar
+docker rm -f $service-service
+docker-compose build $service-service
+docker-compose up -d $service-service
