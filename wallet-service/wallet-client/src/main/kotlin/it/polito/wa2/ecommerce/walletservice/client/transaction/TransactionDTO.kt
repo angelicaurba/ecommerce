22e568707b1e42b2b1c88f4c4904cc107a3467b1
Roberto Bressani <roberto.bressani97@gmail.com>
2021-09-13 11:09:13 +0200

    src: updated transaction to support date

--- wallet-service/wallet-client/src/main/kotlin/it/polito/wa2/ecommerce/walletservice/client/transaction/TransactionDTO.kt
+++ wallet-service/wallet-client/src/main/kotlin/it/polito/wa2/ecommerce/walletservice/client/transaction/TransactionDTO.kt
@@ -1,12 +1,13 @@
 package it.polito.wa2.ecommerce.walletservice.client.transaction
 
 import java.math.BigDecimal
+import java.time.LocalDateTime
 
 data class TransactionDTO(
     val id: Long,
     val fromWallet: String?,
     val toWallet: String,
     val amount: BigDecimal,
-    val timestamp: Long,
+    val timestamp: LocalDateTime,
     val reason:String
 )
