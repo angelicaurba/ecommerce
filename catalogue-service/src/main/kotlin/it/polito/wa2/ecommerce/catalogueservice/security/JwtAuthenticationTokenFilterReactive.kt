ae3eb23cd73f760dbbda4368cc860039949a2ce1
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-09-16 18:38:15 +0200

    catalogue-service: add security for catalogue, still missing security for all the other services

---
+++ catalogue-service/src/main/kotlin/it/polito/wa2/ecommerce/catalogueservice/security/JwtAuthenticationTokenFilterReactive.kt
@@ -0,0 +1,40 @@
+package it.polito.wa2.ecommerce.catalogueservice.security
+
+import it.polito.wa2.ecommerce.common.constants.JWT_HEADER_NAME
+import it.polito.wa2.ecommerce.common.constants.JWT_HEADER_START
+import it.polito.wa2.ecommerce.common.security.utils.JwtUtils
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class JwtAuthenticationManager : ReactiveAuthenticationManager {
+    override fun authenticate(authentication: Authentication): Mono<Authentication> {
+        return Mono.just(authentication)
+    }
+}
+
+
+@Component
+class JwtServerAuthenticationConverter(private val jwtUtils: JwtUtils) : ServerAuthenticationConverter {
+    override fun convert(exchange: ServerWebExchange?): Mono<Authentication> {
+        val result = exchange?.request?.headers?.get(JWT_HEADER_NAME)
+            ?.first {
+                it.startsWith(JWT_HEADER_START)
+            }
+            ?.let {
+                val bearer = jwtUtils.getJwtTokenFromHeader(it)
+                if (jwtUtils.validateJwtToken(bearer))
+                    jwtUtils.getDetailsFromJwtToken(bearer)
+                else
+                    null
+            }
+        println("########################### result $result")
+        return Mono.justOrEmpty(result)
+            .map { UsernamePasswordAuthenticationToken(it, null, it.roles) }
+    }
+}
--- catalogue-service/src/main/kotlin/it/polito/wa2/ecommerce/catalogueservice/security/WebSecurityConfig.kt
+++ catalogue-service/src/main/kotlin/it/polito/wa2/ecommerce/catalogueservice/security/WebSecurityConfig.kt
@@ -1,32 +1,72 @@
 package it.polito.wa2.ecommerce.catalogueservice.security
 
+import it.polito.wa2.ecommerce.common.ErrorMessageDTO
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.core.ResolvableType
+import org.springframework.core.codec.Hints
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.codec.json.Jackson2JsonEncoder
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
 import org.springframework.security.web.server.SecurityWebFilterChain
-
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
 
 @Configuration
-@EnableWebFluxSecurity
-class HelloWebfluxSecurityConfig {
+class WebfluxSecurityConfig {
 
     @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    fun springSecurityFilterChain(http: ServerHttpSecurity,
+                                  jwtAuthenticationManager: ReactiveAuthenticationManager,
+                                  jwtAuthenticationConverter: ServerAuthenticationConverter
+    ): SecurityWebFilterChain {
+
+        val authenticationWebFilter = AuthenticationWebFilter(jwtAuthenticationManager)
+        authenticationWebFilter.setServerAuthenticationConverter(jwtAuthenticationConverter)
+
         http
-            .authorizeExchange { exchanges: AuthorizeExchangeSpec ->
-                exchanges
-                    .anyExchange()
-                    .permitAll()
-//                    .authenticated()
-            }
+            .authorizeExchange()
+            .pathMatchers(HttpMethod.GET)
+            .permitAll()
+            .and()
+            .authorizeExchange()
+            .anyExchange()
+            .authenticated()
+            .and()
             .cors().disable()
             .csrf().disable()
-            .httpBasic(withDefaults())
-            .formLogin(withDefaults())
+            .httpBasic().disable()
+            .formLogin().disable()
+            .addFilterAt(
+            authenticationWebFilter,
+            SecurityWebFiltersOrder.AUTHENTICATION)
+            .exceptionHandling()
+            .authenticationEntryPoint{swe: ServerWebExchange, e : Exception ->
+                val body = ErrorMessageDTO(e, HttpStatus.UNAUTHORIZED, swe.request.path.toString())
+                Mono.fromRunnable{
+                    swe.response.statusCode = HttpStatus.UNAUTHORIZED
+                    swe.response.writeWith(
+                        Jackson2JsonEncoder().encode(
+                            Mono.just(body),
+                            swe.response.bufferFactory(),
+                            ResolvableType.forInstance(body),
+                            MediaType.APPLICATION_JSON,
+                            Hints.from(Hints.LOG_PREFIX_HINT, swe.logPrefix)
+                        )
+                    )
+                }
+            }
+
         return http.build()
     }
 }
