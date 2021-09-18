20bc887fd878e0a3d4654399b132b9ec55cf7b77
Luca Giorgino <lucas.10m.lg@gmail.com>
2021-09-18 10:02:31 +0200

    populate DB base

--- warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/WarehouseServerApplication.kt
+++ warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/WarehouseServerApplication.kt
@@ -1,13 +1,30 @@
 package it.polito.wa2.ecommerce.warehouseservice
 
+import it.polito.wa2.ecommerce.warehouseservice.repository.StockRepository
+import it.polito.wa2.ecommerce.warehouseservice.repository.WarehouseRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import java.math.BigDecimal
+import java.util.*
 
 @SpringBootApplication(scanBasePackages = ["it.polito.wa2.ecommerce"])
 @EnableEurekaClient
 class WarehouseServerApplication {
+    @Bean
+    fun populateDB(
+        warehouseRepository: WarehouseRepository,
+        stockRepository: StockRepository
+    ): CommandLineRunner {
+        return CommandLineRunner {
+            stockRepository.deleteAll()
+            warehouseRepository.deleteAll()
 
+            // TODO populate db
+        }
+    }
 }
 
 fun main(args: Array<String>) {
