f7d1c58865b91a91893f96fb76037847a4e7840f
Luca Giorgino <lucas.10m.lg@gmail.com>
2021-09-23 12:07:47 +0200

    warehouse: new authorized feature for admin

--- warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/service/WarehouseService.kt
+++ warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/service/WarehouseService.kt
@@ -2,6 +2,7 @@ package it.polito.wa2.ecommerce.warehouseservice.service
 
 import it.polito.wa2.ecommerce.warehouseservice.client.WarehouseDTO
 import it.polito.wa2.ecommerce.warehouseservice.client.WarehouseRequestDTO
+import it.polito.wa2.ecommerce.warehouseservice.domain.Warehouse
 
 interface WarehouseService {
     fun deleteWarehouseById(warehouseId: String)
@@ -10,4 +11,6 @@ interface WarehouseService {
     fun addWarehouse(warehouseRequest: WarehouseRequestDTO, warehouseId: String? = null): WarehouseDTO
     fun getWarehouseById(warehouseId: String): WarehouseDTO
     fun getAllWarehouses(pageIdx: Int, pageSize: Int): List<WarehouseDTO>
+    fun isAuthorized(warehouseAdminID: String)
+    fun getWarehouseOrThrowException(warehouseId: String) : Warehouse
 }
