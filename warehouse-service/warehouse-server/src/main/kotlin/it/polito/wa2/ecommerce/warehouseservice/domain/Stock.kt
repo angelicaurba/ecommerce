ddcb42de0c9008ddb3e4cc01449ca36a3b924985
Luca Giorgino <lucas.10m.lg@gmail.com>
2021-09-23 13:05:44 +0200

    Warehouse: fixed cascade

--- warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/domain/Stock.kt
+++ warehouse-service/warehouse-server/src/main/kotlin/it/polito/wa2/ecommerce/warehouseservice/domain/Stock.kt
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size
 @Entity
 @Table(uniqueConstraints = [UniqueConstraint(columnNames = ["warehouse_id", "prodotto"])])
 class Stock (
-    @ManyToOne(optional = false, fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "warehouse_id")
     var warehouse: Warehouse,
 
