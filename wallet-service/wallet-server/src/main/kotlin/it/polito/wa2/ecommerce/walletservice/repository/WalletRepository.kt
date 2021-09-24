f4ceae6a21961a86706df01bc9eaefb7aa60c60f
Roberto Bressani <roberto.bressani97@gmail.com>
2021-09-24 20:08:19 +0200

    src: added repository method to filter wallets for admins

--- wallet-service/wallet-server/src/main/kotlin/it/polito/wa2/ecommerce/walletservice/repository/WalletRepository.kt
+++ wallet-service/wallet-server/src/main/kotlin/it/polito/wa2/ecommerce/walletservice/repository/WalletRepository.kt
@@ -2,6 +2,8 @@ package it.polito.wa2.ecommerce.walletservice.repository
 
 import it.polito.wa2.ecommerce.walletservice.domain.Wallet
 import it.polito.wa2.ecommerce.walletservice.domain.WalletType
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
@@ -13,6 +15,9 @@ interface WalletRepository: PagingAndSortingRepository<Wallet, Long> {
     fun findByWalletTypeAndOwner(walletType: WalletType, owner: String): Wallet?
     @Transactional(readOnly = true)
     fun findByIdAndOwnerAndWalletType(id:Long, owner: String, walletType: WalletType):Wallet?
+
+    @Transactional(readOnly = true)
+    fun findByOwnerAndWalletType(owner: String?, walletType: WalletType?, page: Pageable): Page<Wallet>
 
 
 }
