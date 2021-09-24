16828e796ad42e0e79deaec405ce07f20797bdc5
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-09-24 20:25:52 +0200

    order: implement custom query to check if user bought product

--- order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/repository/PurchaseItemRepository.kt
+++ order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/repository/PurchaseItemRepository.kt
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface PurchaseItemRepository : PagingAndSortingRepository<PurchaseItem, Long> {
@@ -19,6 +20,14 @@ interface PurchaseItemRepository : PagingAndSortingRepository<PurchaseItem, Long
                                       @Param("warehouse_id") warehouseId: String)
 
 
-
+    @Transactional(readOnly = true)
+    @Query("""
+        select case when count(PurchaseItem) > 0 then true else false end from PurchaseItem
+        where productId = :product_id and order.buyerId = :buyer_id
+    """)
+    fun haveBuyerIdBoughtProduct(
+        @Param("product_id") productId: String,
+        @Param("buyer_id") buyerId: String
+    ): Boolean
 
 }
