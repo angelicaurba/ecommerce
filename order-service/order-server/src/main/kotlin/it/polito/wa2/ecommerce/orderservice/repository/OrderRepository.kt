18e8c6fdbf8ccf5338cf2a789862c6101039e352
Angelica Urbanelli <46596028+angelicaurba@users.noreply.github.com>
2021-09-24 15:19:24 +0200

    order: fix custom repo method had wrong parameter

--- order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/repository/OrderRepository.kt
+++ order-service/order-server/src/main/kotlin/it/polito/wa2/ecommerce/orderservice/repository/OrderRepository.kt
@@ -2,15 +2,15 @@ package it.polito.wa2.ecommerce.orderservice.repository
 
 import it.polito.wa2.ecommerce.orderservice.domain.Order
 import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
-import java.awt.print.Pageable
+
 
 @Repository
 interface OrderRepository : PagingAndSortingRepository<Order, Long>{
 
     @Transactional(readOnly = true)
-    fun findAllByBuyerId(buyerId : String, page: PageRequest): Page<Order>
+    fun findAllByBuyerId(buyerId : String, page: Pageable): Page<Order>
 }
